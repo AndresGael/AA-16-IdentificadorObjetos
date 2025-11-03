11c90dc8f3b870595c9b025800edb284c2baf30a
Abelardo Aldana Angulo <L23170066@culiacan.tecnm.mx>
2025-11-03 19:52:42 +0000

    uyuyuy

--- sketch.js
+++ sketch.js
@@ -1,14 +1,37 @@
+let clasificador
+let etiqueta
+let confidencia
+let video
+
 function preload() {
   // put preload code here
+  clasificador = ml5.imageClassifier("MobileNet")
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800)
+  createCanvas(640,480)
+  video = createCapture(VIDEO)
+  video.size(640,480)
+  video.hide()
+  clasificarVideo()
+}
+
+function clasificarVideo() {
+  clasificador.classify(video, gotResults)
+}
+
+function gotResults(results) {
+  etiqueta = results[0].label
+  confidencia = results[0].confidence
+  clasificarVideo()
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  image(video,0,0)
+  textSize(32)
+  fill("light green")
+  text(etiqueta,10,20)
+  text(confidencia,10, 60)
 }
