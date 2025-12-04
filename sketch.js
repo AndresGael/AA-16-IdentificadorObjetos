50daa83111087840ebc4af894e8d958216a14d33
Abelardo Aldana Angulo <L23170066@culiacan.tecnm.mx>
2025-12-04 22:15:46 +0000

    nose

--- sketch.js
+++ sketch.js
@@ -1,66 +1,75 @@
-// let clasificador
-let etiqueta
-let confidencia
-let video
-let confianza = 0
-let texto = ""
-
-function preload() {
-  // put preload code here
-  clasificador = ml5.imageClassifier("MobileNet")
-}
-
-function setup() {
-  // put setup code here
-  createCanvas(640,480)
-  video = createCapture(VIDEO)
-  video.size(640,480)
-  video.hide()
-  clasificarVideo()
-}
-
-function clasificarVideo() {
-  clasificador.classify(video, gotResults)
-}
-
-function gotResults(results) {
-  etiqueta = results[0].label
-  confidencia = results[0].confidence
-  clasificarVideo()
-}
-
-function draw() {
-  // put drawing code here
-  image(video,0,0)
-  textSize(32)
-  fill("light green")
-  text(etiqueta,10,20)
-  text(confidencia,10, 60)
-
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  if (confianza > 0.7) {
-    if (confianza === "Pikachu"){ 
-      texto = " ⚡⚡ ¡Es un Pikachu! ⚡⚡"
-    } else if (label === "Medalla") {
-      texto = "¡Es una Medalla! "
-    } else if (label === "Zorro") {
-      texto = " ¡Es un Zorro! "
-    }
-  } else {
-    texto = " No estoy seguro... ";
-  }
-  text (label + " " + confianza, width / 2, height - 4)
-}
-
-function gotResult (error, results) {
-
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-
+// Classifier Variable
+  let classifier;
+  // Model URL
+  let imageModelURL = './my_model/';
   
-}
+  // Video
+  let video;
+  let flippedVideo;
+  // To store the classification
+  let label = "";
+  let confianza = 0;
+  let texto =""
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  }
+
+  function setup() {
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+  }
+
+  function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    if (confianza > 0.7) {
+      if (label ==="Chopper") {
+        texto = "⚡⚡¡Es un Pikachu!⚡⚡";
+      } else if (label ==="Conejo") {
+        texto = "🏅😁¡Es una Medalla!😁🏅";
+      } else if (label === "Spidey") {
+        texto = "🦊🌟¡Es un Zorro!🌟🦊";
+      }
+    } else {
+      texto = "🤔 No estoy seguro... 🤔";
+    }
+    text(texto + " " + confianza, width / 2, height - 4);
+  }
+
+
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+
+  }
+
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    confianza = results[0].confidence.toFixed(4);
+    // Classifiy again!
+    classifyVideo();
+  }
